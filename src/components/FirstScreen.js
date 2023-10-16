2661567c4f5200449de0e5c333dca420b3538dc0
Vasyl-Stashchuk <lil.doggy.rocky8@gmail.com>
2023-10-16 23:22:45 +0300

    change little thing

--- src/components/FirstScreen.js
+++ src/components/FirstScreen.js
@@ -1,29 +1,37 @@
-import React, {useContext, useEffect} from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import '../css/WtfNav.css';
 import '../css/FirstScreen.css';
 import logo from '../assets/Kronk.svg';
+// import background from '../assets/';
 import {observer} from "mobx-react-lite";
 import {fetchBrands, fetchDevices, fetchTypes} from "../http/deviceApi";
 import {Context} from "../index";
 import {useHistory} from "react-router-dom"; // Імпортуємо зображення для відображення рейтингу.
 
-const FirstScreen = () => {
+const FirstScreen = observer(() => {
+    const [backgroundUrls, setBackgroundUrls] = useState([]);  // Ініціалізація стану для зберігання масиву URL фонових зображень
+    const { deviceStore } = useContext(Context);
 
+    useEffect(() => {
+        fetchDevices().then(data => {
+            // Вибрати 8 випадкових зображень
+            const shuffled = data.rows.sort(() => 0.5 - Math.random());
+            const selectedImages = shuffled.slice(0, 8).map(item => process.env.REACT_APP_API_URL + item.img);
+            setBackgroundUrls(selectedImages);
+        });
+    }, []);
 
     return (
         <div className="first-screen">
-
             <article className="first-img">
                 <img src={logo} alt="MainLogo"/>
             </article>
 
-            <div className="first-item"
-                 style={{ backgroundImage: `url({})` }}  // Виправлено встановлення backgroundImage
-            >
-            </div>
-            
+            {backgroundUrls.map((url, index) => (
+                <div key={index} className="first-item" style={{ backgroundImage: `url(${url})` }}></div>
+            ))}
         </div>
     );
-};
+});
 
 export default FirstScreen;
